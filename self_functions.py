bdd55e17fb9d9972d8124b4f262aeb6b84cc6cf1
Leander <74681179+Sapje@users.noreply.github.com>
2024-09-30 15:30:34 +0200

    Cloned Lars

---
+++ self_functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+
+def histplot(x):
+    sns.histplot(x=x)
+    plt.show()
+    
+def corrplot(df):
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(df.corr(), annot=True)
+    plt.show()
+    
+def sta_sca(df, cols):
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    for i in cols:
+        df[i] = sc.fit_transform(df[[i]])
