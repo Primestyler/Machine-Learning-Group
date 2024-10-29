1750f1eeb827a9ad266f809b96644cd29aa99ffd
Lars Timmer <lars.l.timmer@gmail.com>
2024-10-29 14:40:27 +0100

    Adapted to OOP and PEP-8

--- self_functions.py
+++ self_functions.py
@@ -1,25 +1,24 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import missingno as msno
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
-def histplot(x):
-    sns.histplot(x=x)
-    plt.show()
-    
-def corrplot(df, col, method='pearson'):
-    corrs = df.corr(method=method)[col]
-    
-    # Create a heatmap
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(corrs.to_frame(), annot=True, cmap='coolwarm', cbar=True)
-    plt.title(f'Correlation of {col} with other variables')
-    plt.show()
-    
-def sta_sca(sc, df, cols):
-    for i in cols:
-        df[i] = sc.fit_transform(df[[i]])
-        
-def z_score(x):
-    return (x - x.mean()) / x.std()
+class DataVisualizer:
+    @staticmethod
+    def histplot(df):
+        sns.histplot(df)
+        plt.show()
+
+    @staticmethod
+    def corrplot(df, target_column, method='pearson'):
+        corrs = df.corr(method=method)[target_column]
+        sns.heatmap(corrs.to_frame(), annot=True)
+        plt.show()
+
+class DataPreprocessor:
+    def __init__(self):
+        self.scaler = StandardScaler()
+
+    def scale_features(self, df, columns):
+        df[columns] = self.scaler.fit_transform(df[columns])
+        return df[columns]
