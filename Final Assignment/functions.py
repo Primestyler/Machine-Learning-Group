ea174f8c551f92c77b135f121074b88aca1c49a5
MatthiasJeneW <matthias1.jenewein@gmail.com>
2025-01-01 19:43:53 +0100

    feature engineering first part

--- Final Assignment/functions.py
+++ Final Assignment/functions.py
@@ -1,6 +1,6 @@
 import librosa
 import pandas
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor, as_completed
@@ -62,7 +62,7 @@ class DataLoader:
                     
         for file, data in dataframes.items():
             if len(data) < max_length:
-                dataframes[file] = numpy.pad(data, (0, max_length - len(data)), constant_values=numpy.nan)
+                dataframes[file] = np.pad(data, (0, max_length - len(data)), constant_values=np.nan)
                 
         return pandas.DataFrame(dataframes)
     
@@ -105,6 +105,49 @@ class DataLoader:
 
         return y, sr
     
+    def extract_features(file_path, sr=22050):
+
+        y, sr = librosa.load(file_path, sr=22050)
+
+        spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spectral_centroid_mean = np.mean(spectral_centroid)
+
+        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        spectral_bandwidth_mean = np.mean(spectral_bandwidth)
+
+        zcr = librosa.feature.zero_crossing_rate(y)
+        zcr_mean = np.mean(zcr)
+
+        rms = librosa.feature.rms(y=y)
+        rms_mean = np.mean(rms)
+
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=0.85)
+        rolloff_mean = np.mean(rolloff)
+
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+        mfcc_means = [np.mean(coeff) for coeff in mfcc]
+        
+        chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+        chroma_means = [np.mean(chroma_coeff) for chroma_coeff in chroma]
+
+        tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+    
+        features = {
+            'spectral_centroid': spectral_centroid_mean,
+            'spectral_bandwidth': spectral_bandwidth_mean,
+            'zero_crossing_rate': zcr_mean,
+            'rms': rms_mean,
+            'spectral_rolloff': rolloff_mean,
+            'mfcc_mean_1': mfcc_means[0],
+            'mfcc_mean_2': mfcc_means[1],
+            'mfcc_mean_3': mfcc_means[2],
+            'chroma_mean_1': chroma_means[0],
+            'chroma_mean_2': chroma_means[1],
+            'tempo': tempo
+        }
+    
+        return features
+
 class Visualizer:
     def __init__(self, data: pandas.DataFrame, title: str, rows: int, cols: int, figsize: tuple = (20, 20)):
         """Initializer for the Visualizer class
