25502dfb18ce08540dbb6d8e91a416d2a8bb16ec
Lars Timmer <lars.l.timmer@gmail.com>
2025-01-11 09:50:27 +0100

    Created KMeans + predictions

--- Final Assignment/functions.py
+++ Final Assignment/functions.py
@@ -70,7 +70,7 @@ class DataLoader:
                 'mfcc_mean_3': mfcc_means[2],
                 'chroma_mean_1': chroma_means[0],
                 'chroma_mean_2': chroma_means[1],
-                'tempo': tempo,
+                'tempo': tempo[0],
                 'contrast_mean_1': contrast_means[0],
                 'contrast_mean_2': contrast_means[1],
                 'tonnetz_mean_1': tonnetz_means[0],
@@ -120,3 +120,74 @@ class DataLoader:
                     print(f"Error loading {file_name}: {e}")
 
         return pd.DataFrame(feature_array)
+    
+
+class KMeansClustering:
+    def __init__(self, unlabeled_data, unlabled_df):
+        self.unlabeled_data = unlabeled_data
+        self.unlabeled_df = unlabled_df
+        
+    def finding_k(self, k_range):
+        inertia = []
+        
+        for k in range(k_range[0], k_range[1]):
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            kmeans.fit(self.unlabeled_data)
+            inertia.append(kmeans.inertia_)
+            
+        self._plot_elbow(k_range, inertia)
+        
+    def create_kmeans(self, k):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(self.unlabeled_data)
+        self.unlabeled_df['cluster'] = kmeans.labels_
+        
+        return self.unlabeled_df
+    
+    def cluster_to_genre(self, genre_mapping):
+        self.unlabeled_df['genre'] = self.unlabeled_df['cluster'].map(genre_mapping)
+    
+    def create_submission(self):
+        submission = self.unlabeled_df[['filename', 'genre']]
+        submission.to_csv(f'submission_{datetime.datetime.now().strftime("%H:%M:%S")}.csv', index=False)
+    
+    def _plot_elbow(self, k_range, inertia):
+        plt.figure(figsize=(10, 5))
+        plt.plot(range(k_range[0], k_range[1]), inertia, marker='o')
+        plt.title('Elbow Method')
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Inertia')
+        plt.show()
+        
+    def _map_clusters_to_genres(self):
+        cluster_genre_mapping = (self.labeled_df.groupby('cluster')['genre'].agg(lambda x:x.mode()[0]).to_dict())
+        
+        return cluster_genre_mapping
+    
+class PostClusteringVisualizations:
+    def __init__(self, clustered_df, labeled_df):
+        self.clustered_df = clustered_df
+        self.labeled_df = labeled_df
+        self.genres = self.labeled_df['genre'].unique()
+        
+    def scatter_plot(self, col1, col2):
+        fig, axes = plt.subplots(5, 2, figsize=(10, 20))
+        axes = axes.flatten()
+        
+        for i, genre in enumerate(self.genres):
+            ax = axes[i]
+            genre_df = self.labeled_df[self.labeled_df['genre'] == genre]
+            
+            sns.scatterplot(data=genre_df, x=col1, y=col2, color='red', ax=ax)
+            sns.scatterplot(data=self.clustered_df, x=col1, y=col2, hue='cluster', palette='viridis', marker='o', ax=ax)
+            
+            ax.set_title(genre)
+            
+        for j in range(len(self.genres), len(axes)):
+            fig.delaxes(axes[j])
+            
+        fig.suptitle(f'Clustered Data and Genre Comparison: {col1} vs {col2}', fontsize=16)
+        plt.tight_layout(rect=[0, 0, 1, 0.99])
+        plt.show()
+        
+        
